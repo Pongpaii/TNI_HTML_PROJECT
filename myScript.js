28e6be3235d82d9bab66ab61d2549411a713d640
Pongpaii <onekickman11@gmail.com>
2024-07-20 07:26:51 +0700

    first commit

---
+++ myScript.js
@@ -0,0 +1,6 @@
+  document.addEventListener("DOMContentLoaded", function() {
+            const animatedElement = document.querySelector(".animate-on-load");
+            animatedElement.style.opacity = "1"; // เปลี่ยนความทึบเป็น 1
+            animatedElement.style.transform = "translateY(0)"; // เลื่อนกลับไปยังตำแหน่งเดิม
+        });
+
